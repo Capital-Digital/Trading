9d72d84211e5a700b9f1e4d3157629f89fd33c2d
Florent <f.wozniak@protonmail.ch>
2021-06-28 11:13:42 +0200

    bug

--- trading/models.py
+++ trading/models.py
@@ -134,12 +134,12 @@ class Account(models.Model):
         if target is not None:
             # Restore previously saved target value and quantity to avoid instability
             # at the end of the rebalancing with a lot of orders with small amount
-            df[('target', 'value')] = target.loc[df.index, 'value']
-            df[('target', 'quantity')] = target.loc[df.index, 'quantity']
+            df['target']['value'] = target.loc[df.index, 'value']
+            df['target']['quantity'] = target.loc[df.index, 'quantity']
         else:
             # Calculate target
-            df['target', 'value'] = df.target.percent * balance * float(self.leverage)
-            df[('target', 'quantity')] = df.target.value / df.price.hourly
+            df['target']['value'] = df.target.percent * balance * float(self.leverage)
+            df['target']['quantity'] = df.target.value / df.price.hourly
 
         # Insert delta
         df[('delta', 'value')] = df.exposure.total_value - df.target.value

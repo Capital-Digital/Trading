f80961882f5dc1d05a57e93e2120d605d962df8e
Florent <f.wozniak@protonmail.ch>
2021-06-30 20:08:07 +0200

    bug

--- trading/tasks.py
+++ trading/tasks.py
@@ -3054,7 +3054,7 @@ def rebalance(strategy_id, account_id=None):
                 log.exception('While loop failed: {0} {1}'.format(type(e).__name__, str(e)),
                               symbol=market.symbol,
                               wallet=market.default_type,
-                              exc_info=traceback.format_exc()
+                              exc_info=traceback.print_exc()
                               )
 
     # Configure websocket client for wallet

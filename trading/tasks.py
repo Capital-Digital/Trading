89dbe6bb36358fdb4d0fda1a1a0c12ae4139ca1c
Florent <f.wozniak@protonmail.ch>
2021-07-28 10:05:38 +0200

    bug

--- trading/tasks.py
+++ trading/tasks.py
@@ -1152,11 +1152,10 @@ def rebalance(strategy_id, account_id=None):
                 # Calculate distance in % to the best bid or to the best ask
                 distance = abs(average_price / book[0][0] - 1)
 
-                if distance is None:
-                    print('book', book)
-                    print('average_price', average_price)
-                    print('weights', weights)
-                    print('ob', ob)
+                print('book', book)
+                print('average_price', average_price)
+                print('weights', weights)
+                print('ob', ob)
 
                 return distance
 

a0822c68b1b25ce3889be3b36ecf5f0e6174829e
Florent <f.wozniak@protonmail.ch>
2021-10-02 15:11:54 +0200

    fix

--- marketsdata/admin.py
+++ marketsdata/admin.py
@@ -378,7 +378,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ('currency', 'year', 'semester', 'count_timestamp')
     readonly_fields = ('name', 'currency', 'year', 'semester', 'dt_created', 'data')
     list_filter = ('currency__code', 'year',)
-    ordering = ('year', 'currency')
+    ordering = ('-year', 'currency', '-semester', )
     save_as = True
 
     def count_timestamp(self, obj):

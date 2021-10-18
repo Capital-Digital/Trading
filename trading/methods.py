da15a4fdf965ff52d92cfd9b553808589786bfff
Florent <f.wozniak@protonmail.ch>
2021-10-18 10:19:40 +0200

    fix

--- trading/methods.py
+++ trading/methods.py
@@ -16,7 +16,7 @@ dt = timezone.now().replace(minute=0, second=0, microsecond=0) - timedelta(hours
 
 
 def convert_balance(row, exchange):
-    print(row)
+    print(row.T)
     price = Currency.objects.get(code=row.index).get_latest_price(exchange)
     return row.value * price
 

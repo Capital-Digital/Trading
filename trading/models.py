c7abb8f5fb810292fe3943fdf80481e47ab8db9a
Florent <f.wozniak@protonmail.ch>
2021-10-18 20:40:51 +0200

    fix

--- trading/models.py
+++ trading/models.py
@@ -161,12 +161,14 @@ class Account(models.Model):
         target = self.get_target_qty()
         df = self.balances.loc[:, self.balances.columns.get_level_values(2) == 'quantity']
         df = df.droplevel([1, 2], axis=1)
-
+        print(target)
         for coin_target in target.index:
             for coin_account in df.index:
                 for source in df.columns:
                     if coin_target == coin_account:
-                        print(coin_account, df.loc[coin_account, source])
+                        target[coin_target] -= df.loc[coin_account, source]
+
+        print(target)
 
     ##############################################################################################
     # Construct a dataframe with wallets balance, positions, exposure and delta

c0ce2dd0f247836a3284f6eb2f0e54023d39c813
Florent <f.wozniak@protonmail.ch>
2021-10-18 15:51:03 +0200

    fix

--- trading/models.py
+++ trading/models.py
@@ -73,7 +73,7 @@ class Account(models.Model):
                           columns=pd.MultiIndex.from_product([['quantity'], [key], [wallet]])
                           )
         self.balances = df if not hasattr(self, 'balances') else pd.concat([self.balances, df])
-        self.balances = self.balances.groupby(level=0).mean()
+        self.balances = self.balances.groupby(level=0).last()
         return self.balances
 
     # Return a dictionary with balance of a specific wallet

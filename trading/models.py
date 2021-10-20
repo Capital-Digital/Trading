db5aa24e61d59cbc6075ae39573629bd2959c555
Florent <f.wozniak@protonmail.ch>
2021-10-20 13:16:13 +0200

    fix

--- trading/models.py
+++ trading/models.py
@@ -205,7 +205,7 @@ class Account(models.Model):
                 amount = delta
 
             price = Currency.objects.get(code=code).get_latest_price(self.exchange)
-            price += (price * self.limit_price_tolerance)
+            price += (price * float(self.limit_price_tolerance))
             market = Market.objects.get(quote__code=self.exchange.dollar_currency,
                                         exchange=self.exchange,
                                         base__code=code,

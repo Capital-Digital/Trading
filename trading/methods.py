976e8437a0a28f0a8482b7dfa39b6e690f26551d
Florent <f.wozniak@protonmail.ch>
2021-10-22 12:28:00 +0200

    fix

--- trading/methods.py
+++ trading/methods.py
@@ -20,7 +20,7 @@ dt = timezone.now().replace(minute=0, second=0, microsecond=0) - timedelta(hours
 def convert_balance(row, wallet, exchange):
     price = Currency.objects.get(code=row.name).get_latest_price(exchange)
     print(row)
-    for key in row[wallet].columns:
+    for key in row.index.get_level_values(1):
         print(key)
         if row[wallet][key]['quantity']:
             return row[wallet][key]['quantity'] * price

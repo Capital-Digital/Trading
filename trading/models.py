b1ec94dc0d787f0f87b5f67c3f2c4f2f3e9efd5c
Florent <f.wozniak@protonmail.ch>
2021-10-22 12:49:51 +0200

    fix

--- trading/models.py
+++ trading/models.py
@@ -92,8 +92,7 @@ class Account(models.Model):
             balances_qty = self.get_balances_qty(wallet)
             if balances_qty is not None:
                 df = balances_qty.apply(lambda row: convert_balance(row, wallet, self.exchange), axis=1)
-                return df
-                df.columns.set_levels(['value' for i in range(len(df.columns))], level=1,inplace=True)
+                df.columns.set_levels(['value'], level=1,inplace=True)
                 self.balances = pd.concat([self.balances, df])
                 self.balances = self.balances.groupby(level=0).last()
 

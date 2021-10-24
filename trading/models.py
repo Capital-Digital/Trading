6629b124ace185652ca534047ffc53df2f8d9cef
Florent <f.wozniak@protonmail.ch>
2021-10-24 09:26:42 +0200

    fix

--- trading/models.py
+++ trading/models.py
@@ -93,7 +93,7 @@ class Account(models.Model):
         for wallet in self.exchange.get_wallets():
             balances_qty = self.get_balances_qty(wallet)
             if wallet in balances_qty.columns.get_level_values(0):
-                if 'value' not in balances_qty.columns.get_level_values(1):
+                if df.index.nlevels == 2:
                     print(balances_qty)
 
                     log.info('Get balances value ({0})'.format(wallet))

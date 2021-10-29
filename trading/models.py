37e1db93e8d49b84f0c86286e4195a901a953f26
Florent <f.wozniak@protonmail.ch>
2021-10-29 10:46:07 +0200

    fix

--- trading/models.py
+++ trading/models.py
@@ -36,7 +36,7 @@ class Account(models.Model):
     objects = models.Manager()
     name = models.CharField(max_length=100, null=True, blank=False)
     exchange = models.ForeignKey(Exchange, on_delete=models.SET_NULL, related_name='account', blank=True, null=True)
-    strategies = models.ManyToManyField(Strategy, related_name='account', null=True)
+    strategies = models.ManyToManyField(Strategy, related_name='account')
     params = models.JSONField(null=True, blank=True)
     valid_credentials = models.BooleanField(null=True, default=None)
     trading = models.BooleanField(null=True, blank=False, default=False)

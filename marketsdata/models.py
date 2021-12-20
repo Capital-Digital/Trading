a47e5a52bc2cec5e45da2b21b158002bade5b381
Florent <f.wozniak@protonmail.ch>
2021-12-21 00:31:47 +0100

    fix

--- marketsdata/models.py
+++ marketsdata/models.py
@@ -499,9 +499,11 @@ class Exchange(models.Model):
                     # Append row if code in dataframe else create new column
                     axis = 0 if i.market.base.code in list(df.columns) else 1
                     vo = pd.concat([vo, tmp_v], axis=axis)
-                    print('vo1\n', vo)
+                    if i.market.base.code == 'ETH':
+                        print('vo1\n', vo)
                     vo = vo.groupby(level=0).mean()
-                    print('vo2\n', vo)
+                    if i.market.base.code == 'ETH':
+                        print('vo2\n', vo)
 
             # Set timestamp at the end of the period (same as candles)
             df = df.shift(-1, freq='H')

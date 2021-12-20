43cf054ff1daca8850394acc2cb606b147ddbda7
Florent <f.wozniak@protonmail.ch>
2021-12-20 22:43:02 +0100

    fix

--- marketsdata/models.py
+++ marketsdata/models.py
@@ -483,15 +483,12 @@ class Exchange(models.Model):
                 # Select data and create dataframes
                 last = [e['last'] for e in data]
                 tmp_l = pd.DataFrame(last, index=timestamps, columns=[i.market.base.code])
+                tmp_l.drop_duplicates(inplace=True)
                 tmp_l.index = pd.to_datetime(tmp_l.index, unit='s')
                 tmp_l = tmp_l.groupby(tmp_l.index).first().resample('H').fillna('ffill')
 
                 # Append row if code in dataframe else create new column
                 axis = 0 if i.market.base.code in list(df.columns) else 1
-
-                tmp_l.drop_duplicates(inplace=True)
-                df.drop_duplicates(inplace=True)
-
                 df = pd.concat([df, tmp_l], axis=axis)
                 df = df.groupby(level=0).mean()
 

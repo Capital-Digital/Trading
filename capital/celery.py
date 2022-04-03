255e965a31b77bcc9306e15165a8f89c689651ca
Florent <f.wozniak@protonmail.ch>
2022-04-03 12:03:28 +0200

    fix

--- capital/celery.py
+++ capital/celery.py
@@ -5,7 +5,6 @@ import structlog
 from celery import Celery
 from celery.signals import setup_logging
 from django_structlog.celery.steps import DjangoStructLogInitStep
-import capital.settings as settings
 
 from kombu import Queue, Exchange
 from django_structlog.celery import signals
@@ -23,7 +22,7 @@ app = Celery('capital', broker='redis://localhost:6379/0')
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object(settings, namespace='CELERY')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()

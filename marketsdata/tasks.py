6021b334cdea29a659b8b04775edf54aec125738
Florent <f.wozniak@protonmail.ch>
2022-04-03 11:26:56 +0200

    fix

--- marketsdata/tasks.py
+++ marketsdata/tasks.py
@@ -807,7 +807,7 @@ def group_strategy(self, exid):
     from strategy.models import Strategy
     strategies = Strategy.objects.filter(exchange__exid=exid)
 
-    job = group(chain_st_ac.s(strategy.id) for strategy in strategies)()
+    job = group(chain_st_ac.s(strategy.id) for strategy in strategies)
     res = job.apply_async()
 
     with allow_join_result():

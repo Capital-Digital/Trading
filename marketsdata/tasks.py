d78c2983df450dde24f7d083cd412c82c386ad56
Florent <f.wozniak@protonmail.ch>
2022-04-10 23:30:28 +0200

    fix

--- marketsdata/tasks.py
+++ marketsdata/tasks.py
@@ -925,6 +925,7 @@ def update_dataframe(self, exid):
                 tmp.columns = pd.MultiIndex.from_product([tmp.columns, [code]])
                 df = pd.concat([df, tmp], axis=1)
 
+            print(df.index.duplicated(keep='first'))
             df = df[~df.index.duplicated(keep='first')]
             # df.index = df.index.drop_duplicates(keep='first')
             df = df.reindex(sorted(df.columns), axis=1)

33cf748823c6c30f2e01b1eb123251fa9b6f2ea6
Florent <f.wozniak@protonmail.ch>
2022-04-10 17:54:43 +0200

    fix

--- marketsdata/models.py
+++ marketsdata/models.py
@@ -202,7 +202,7 @@ class Exchange(models.Model):
     def get_strategies_codes(self):
         from strategy.models import Strategy
         strategies = Strategy.objects.filter(exchange__exid=self.exid)
-        return list(itertools.chain([s.get_codes_long() for s in strategies]))[0]
+        return list(itertools.chain([s.get_codes_long() for s in strategies]))
 
     # Return True if there is available credit
     def has_credit(self, wallet=None):

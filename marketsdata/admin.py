10afbb9144a7bd73e02c35aed6acfe4594b893ec
Florent <f.wozniak@protonmail.ch>
2022-04-11 21:30:14 +0200

    fix

--- marketsdata/admin.py
+++ marketsdata/admin.py
@@ -46,8 +46,9 @@ class CustomerAdmin(admin.ModelAdmin):
 
     def get_df_latest_index(self, obj):
         if hasattr(obj, 'data'):
-            if obj.data:
-                return list(obj.data.index[-1])[0]
+            if isinstance(obj, pd.DataFrame):
+                if not obj.data.empty:
+                    return list(obj.data.index[-1])[0]
 
     get_df_latest_index.short_description = "Last index"
 

02a481481f602dc63aade66357dc654482a532da
Florent <f.wozniak@protonmail.ch>
2022-04-11 10:08:18 +0200

    fix

--- marketsdata/tasks.py
+++ marketsdata/tasks.py
@@ -857,7 +857,7 @@ def run_account(self, account_id):
 
 # Update dataframes of all exchanges
 @app.task(bind=True, name='Update_exchanges')
-def update_exchanges(self, signal):
+def update_exchanges(self, signal=True):
     exchanges = Exchange.objects.filter(enable=True)
     for exchange in exchanges:
         update_dataframe.delay(exchange.exid, signal)

ac4aaffcd97ad363bc17628a167e9f0b62878bb1
Florent <f.wozniak@protonmail.ch>
2022-04-14 14:52:59 +0200

    fix

--- trading/models.py
+++ trading/models.py
@@ -1285,13 +1285,13 @@ class Account(models.Model):
         current = self.balances.account.current.percent
         for coin, val in current[current != 0].sort_values(ascending=False).items():
             if coin != self.quote:
-                log.info('Percentage for {0}: {1}%'.format(coin, round(val, 1)))
+                log.info('Percentage for {0}: {1}%'.format(coin, round(val*100, 1)))
 
         log.info(' ')
 
         target = self.balances.account.target.percent
         for coin, val in target[target != 0].sort_values(ascending=False).items():
-            log.info('Target for {0}: {1}%'.format(coin, round(val, 1)))
+            log.info('Target for {0}: {1}%'.format(coin, round(val*100, 1)))
 
     # Mark the account as currently trading (busy) or not
     def set_busy_flag(self, busy):

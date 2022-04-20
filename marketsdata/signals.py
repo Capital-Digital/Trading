6509f076639466801ac3504f039a04e4e4139ebc
Florent <f.wozniak@protonmail.ch>
2022-04-20 14:15:00 +0200

    fix

--- marketsdata/signals.py
+++ marketsdata/signals.py
@@ -2,6 +2,7 @@ import time
 import structlog
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from marketsdata.models import Exchange
 from strategy.tasks import bulk_update_strategies
 from celery.signals import task_success, task_postrun, task_failure
 
@@ -12,17 +13,13 @@ log = structlog.get_logger(__name__)
 def task_postrun_handler(task_id=None, task=None, args=None, state=None, retval=None, **kwargs):
 
     if task.name == 'Markets_____Update_dataframe':
-
         exid, tickers = args
         if state == 'SUCCESS':
-
-            # Execute strategies async
             bulk_update_strategies.delay(exid, trade=True)
 
         else:
             log.error('Dataframe update failure')
 
     if task.name == 'Markets_____Update_exchange_prices':
-
         if state == 'SUCCESS':
             pass

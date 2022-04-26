a23d50173a1fb0a2d8ac507710bd51c559f27e02
Florent <f.wozniak@protonmail.ch>
2022-04-26 13:36:54 +0200

    fix

--- trading/urls.py
+++ trading/urls.py
@@ -4,22 +4,5 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index_trading, name='index'),
 ]
-#
-#
-# urlpatterns = [
-#     # path('', index, name='index'),
-#     path("", dashboard, name="dashboard"),
-#
-#     url(r"^oauth/", include("social_django.urls")),
-#     url(r"^register/", register, name="register"),
-#     url(r"^login/", login, name='login'),
-#
-#     path("see_request/", see_request),
-#     path("user_info/", user_info),
-#     path("private_place/", private_place),
-#     path("accounts/", include("django.contrib.auth.urls")),
-#     path("add_messages/", add_messages),
-#
-# ]

6c0fe1ad0cc4bf8bb4795fa40b62ce33de00ded0
Florent <f.wozniak@protonmail.ch>
2022-04-26 12:54:22 +0200

    fix

--- trading/views.py
+++ trading/views.py
@@ -1,5 +1,23 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from trading.models import Account, Order, Fund, Position
 
 
 def index(request):
-    return HttpResponse()
+    """View function for home page of site."""
+
+    # Generate counts of some main objects
+    num_accounts = Account.objects.all().count()
+    num_open_orders = Order.objects.filter(status='open').count()
+    num_closed_orders = Order.objects.filter(status='closed').count()
+    num_canceled_orders = Order.objects.filter(status='canceled').count()
+
+    context = {
+        'num_accounts': num_accounts,
+        'num_open_orders': num_open_orders,
+        'num_closed_orders': num_closed_orders,
+        'num_canceled_orders': num_canceled_orders,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)

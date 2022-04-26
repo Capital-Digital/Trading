32c164efbac3794129d2146c18326f8c58d22e39
Florent <f.wozniak@protonmail.ch>
2022-04-26 15:40:04 +0200

    fix

--- capital/urls.py
+++ capital/urls.py
@@ -13,8 +13,6 @@ urlpatterns = [
     path('trading/', include('trading.urls')),
 
     path('admin/', admin.site.urls),
-
-    url(r"^", include("marketsdata.urls"), name='marketsdata'),
     url(r"^", include("trading.urls"), name='trading'),
 
     path("graphql", GraphQLView.as_view(graphiql=True)),

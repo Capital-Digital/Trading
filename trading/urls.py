7987c9e55b2a465f5c0a19f3b44b7ca63456d92f
Florent <f.wozniak@protonmail.ch>
2022-04-26 14:45:13 +0200

    fix

--- trading/urls.py
+++ trading/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from django.conf.urls import url, include
 
-from trading.views import index
+from trading.views import trading_stats
 
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', trading_stats, name='index'),
 ]

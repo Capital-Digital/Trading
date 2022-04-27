87d575d7675ebd8d7ee4fa332266c6386082c4c9
Florent <f.wozniak@protonmail.ch>
2022-04-27 13:56:17 +0200

    fix

--- marketsdata/urls.py
+++ marketsdata/urls.py
@@ -5,4 +5,6 @@ from marketsdata.views import marketsdata_stats
 
 urlpatterns = [
     path('exchanges', marketsdata_stats, name='exchanges'),
+    path('markets', marketsdata_stats, name='markets'),
+    path('currencies', marketsdata_stats, name='currencies'),
 ]

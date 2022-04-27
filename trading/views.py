a5ac8b9d9416806073df52a3df391898d347f9d3
Florent <f.wozniak@protonmail.ch>
2022-04-27 11:50:19 +0200

    fix

--- trading/views.py
+++ trading/views.py
@@ -9,7 +9,7 @@ from django.views import generic
 from django.shortcuts import get_object_or_404
 from trading.tables import OrderTable
 from django_tables2 import SingleTableMixin, LazyPaginator
-from datetime import timedelta, timezone
+import datetime
 
 
 class AccountListView(generic.ListView):
@@ -34,8 +34,8 @@ class AccountDetailView(SingleTableMixin, generic.DetailView):
         table.paginator_class = LazyPaginator
         table.localize=True
 
-        now = timezone.now()
-        last_24h = now - timedelta(days=1)
+        now = datetime.timezone.now()
+        last_24h = now - datetime.timedelta(days=1)
 
         context['table'] = table
         context['assets_value'] = round(self.object.assets_value(), 2)

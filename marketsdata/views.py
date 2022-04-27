a485bae10ba3f62c5dab60da8bed79afa12b3942
Florent <f.wozniak@protonmail.ch>
2022-04-27 13:59:54 +0200

    fix

--- marketsdata/views.py
+++ marketsdata/views.py
@@ -2,22 +2,53 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.response import TemplateResponse
 from marketsdata.models import Market, Exchange, Currency
+from django.views import generic
 
 
-def marketsdata_stats(request):
+class ExchangeListView(generic.ListView):
+    model = Exchange
+    paginate_by = 10
+    context_object_name = 'exchanges_list'
+    template_name = 'exchanges.html'
 
-    # Generate counts of some main objects
-    num_exchanges = Exchange.objects.count()
-    num_currencies = Currency.objects.count()
-    num_spot_markets = Market.objects.filter(type='spot').count()
-    num_derivative_markets = Market.objects.filter(type='derivative').count()
+    def get_queryset(self):
+        return Exchange.objects.all()
 
-    context = {
-        'num_exchanges': num_exchanges,
-        'num_currencies': num_currencies,
-        'num_spot_markets': num_spot_markets,
-        'num_derivative_markets': num_derivative_markets,
-    }
 
-    # Render the HTML template index.html with the data in the context variable
-    return TemplateResponse(request, 'exchanges.html', context=context)
+class MarketListView(generic.ListView):
+    model = Market
+    paginate_by = 10
+    context_object_name = 'markets_list'
+    template_name = 'markets.html'
+
+    def get_queryset(self):
+        return Market.objects.all()
+
+
+class CurrencyListView(generic.ListView):
+    model = Currency
+    paginate_by = 10
+    context_object_name = 'currencies_list'
+    template_name = 'currencies.html'
+
+    def get_queryset(self):
+        return Currency.objects.all()
+
+
+# def marketsdata_stats(request):
+#
+#     # Generate counts of some main objects
+#     num_exchanges = Exchange.objects.count()
+#     num_currencies = Currency.objects.count()
+#     num_spot_markets = Market.objects.filter(type='spot').count()
+#     num_derivative_markets = Market.objects.filter(type='derivative').count()
+#
+#     context = {
+#         'num_exchanges': num_exchanges,
+#         'num_currencies': num_currencies,
+#         'num_spot_markets': num_spot_markets,
+#         'num_derivative_markets': num_derivative_markets,
+#     }
+#
+#     # Render the HTML template index.html with the data in the context variable
+#     return TemplateResponse(request, 'exchanges.html', context=context)

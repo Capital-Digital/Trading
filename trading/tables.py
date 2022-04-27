f80db74896b1613f649a93523b453a622ce7843e
Florent <f.wozniak@protonmail.ch>
2022-04-27 09:25:54 +0200

    fix

--- trading/tables.py
+++ trading/tables.py
@@ -5,8 +5,11 @@ from trading.models import Order
 class OrderTable(tables.Table):
     price = tables.Column(verbose_name='Limit')
 
+    def render_dt_create(self, **kwargs):
+        return round(kwargs['value'], 2)
+
     class Meta:
         model = Order
         fields = ('clientid', 'market__symbol', 'market__type', 'status', 'side', 'action', 'amount', 'price',
-                  'cost', 'dt_create', 'dt_update')
+                  'cost', 'render_dt_create', 'dt_update')
         exclude = ('ID',)

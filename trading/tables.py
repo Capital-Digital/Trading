3f8d4136c2340f9f90651c842d7fe3c80f93f027
Florent <f.wozniak@protonmail.ch>
2022-04-27 09:09:38 +0200

    fix

--- trading/tables.py
+++ trading/tables.py
@@ -4,8 +4,10 @@ from trading.models import Order
 
 class OrderTable(tables.Table):
     id = tables.Column(attrs={"td": {"class": "my-class"}})
+    name = tables.Column(order_by="dt_create")
 
     class Meta:
         model = Order
         fields = ('clientid', 'market__symbol', 'market__type', 'status', 'side', 'action', 'amount', 'price',
                   'cost', 'dt_create', 'dt_update')
+        exclude = ('ID',)

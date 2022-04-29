abcb1b46f4253254992179d6ede484aef4f0ac14
Florent <f.wozniak@protonmail.ch>
2022-04-29 08:38:42 +0200

    fix

--- trading/admin.py
+++ trading/admin.py
@@ -82,7 +82,7 @@ class CustomerAdmin(admin.ModelAdmin):
 
     readonly_fields = ('clientid', 'account', 'market', 'status',  'action', 'type', 'amount', 'side',
                        'cost', 'filled', 'average', 'remaining', 'timestamp', 'max_qty', 'trades',
-                       'last_trade_timestamp', 'price', 'fee', 'datetime', 'response', 'orderid', 'user', 'sender')
+                       'last_trade_timestamp', 'price', 'fee', 'datetime', 'response', 'orderid', 'owner', 'sender')
     actions = ['place_order', 'refresh', 'cancel_order']
 
     list_filter = (

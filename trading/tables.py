781c581bb173881af6386822f4e2aef72b7a9a7b
Florent <f.wozniak@protonmail.ch>
2022-04-29 12:54:04 +0200

    fix

--- trading/tables.py
+++ trading/tables.py
@@ -45,3 +45,27 @@ class AssetTable(tables.Table):
         model = Asset
         fields = ('currency', 'wallet', 'total', 'free', 'used', 'dt_modified')
         exclude = ('ID',)
+
+
+class PositionTable(tables.Table):
+
+    market = tables.Column(verbose_name='Market')
+    quantity = tables.Column(verbose_name='Quantity')
+    side = tables.Column(verbose_name='Side')
+    unrealized_pnl = tables.Column(verbose_name='Unrealized PnL')
+    notional_value = tables.Column(verbose_name='Notional value')
+
+    dt_modified = tables.DateTimeColumn(format='M d Y, h:i:s')
+    dt_modified = tables.Column(verbose_name='Last update')
+
+    def render_notional_value(self, **kwargs):
+        return round(kwargs['value'], 3)
+
+    def render_unrealized_pnl(self, **kwargs):
+        return round(kwargs['value'], 3)
+
+    class Meta:
+        model = Asset
+        fields = ('market', 'quantity', 'side', 'unrealized_pnl', 'notional_value', 'dt_modified')
+        exclude = ('ID',)
+
--- trading/views.py
+++ trading/views.py
@@ -5,7 +5,7 @@ from django.template.response import TemplateResponse
 from trading.models import Account, Order, Fund, Position, Asset
 from django.views import generic
 from django.shortcuts import get_object_or_404
-from trading.tables import OrderTable, AssetTable
+from trading.tables import OrderTable, AssetTable, PositionTable
 from django_tables2 import SingleTableMixin, LazyPaginator
 from django.utils import timezone
 from datetime import timedelta, datetime
@@ -37,7 +37,8 @@ class AccountDetailView(SingleTableMixin, generic.DetailView):
         context = super().get_context_data(**kwargs)
         orders = Order.objects.filter(account=self.object)
 
-        table_asset = AssetTable(Asset.objects.filter(account=self.object).order_by('currency__code'))
+        table_asset = AssetTable(Asset.objects.filter(account=self.object).order_by('wallet'))
+        table_position = PositionTable(Position.objects.filter(account=self.object).order_by('market__symbol'))
 
         table_order = OrderTable(Order.objects.filter(account=self.object).order_by('-dt_created'))
         table_order.paginate(page=self.request.GET.get("page", 1), per_page=10)
@@ -48,6 +49,7 @@ class AccountDetailView(SingleTableMixin, generic.DetailView):
         last_24h = now - timedelta(hours=6)
 
         context['table_asset'] = table_asset
+        context['table_position'] = table_position
         context['table_order'] = table_order
         context['owner'] = self.object.owner
         context['assets_value'] = round(self.object.assets_value(), 2)

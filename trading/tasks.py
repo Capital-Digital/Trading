ddaca1eaaac6c6c3b082983452483b43be55fffb
Florent <f.wozniak@protonmail.ch>
2022-04-30 19:57:31 +0200

    fix

--- trading/tasks.py
+++ trading/tasks.py
@@ -306,10 +306,7 @@ def rebalance(account_id, reload=False, release=True):
     current = account.balances.account.current.percent
     for coin, val in current[current != 0].sort_values(ascending=False).items():
         log.info('Percentage for {0}: {1}%'.format(coin, round(val * 100, 1)), account=account.name)
-
-    exposure = account.balances.account.current.exposure
-    for coin, val in exposure[exposure != 0].sort_values(ascending=False).items():
-        log.info('-> {0} {1}'.format(round(val, 3), coin), account=account.name)
+        log.info('-> {0} {1}'.format(round(account.balances.account.current.exposure, 3), coin), account=account.name)
 
     # Display target percent
     target = account.balances.account.target.percent

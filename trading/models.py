2cd0d8f7daab46ae9feb3a6e1bdb3035d9914117
Florent <f.wozniak@protonmail.ch>
2022-05-01 00:53:09 +0200

    fix

--- trading/models.py
+++ trading/models.py
@@ -862,7 +862,7 @@ class Account(models.Model):
         pass
 
     # Return sum of open orders size
-    def get_open_orders_spot(self, code, side, actions):
+    def get_open_orders_spot(self, code, side, action):
 
         # Test if a close_spot or a buy_spot order is open
         market, flip = self.exchange.get_spot_market(code, self.quote)
@@ -870,7 +870,7 @@ class Account(models.Model):
                                   status__in=['open', 'preparation'],
                                   market=market,
                                   side=side,
-                                  action__in=actions
+                                  action=action
                                   )
         if not qs.exists():
             log.info('No pending order found for {0} in spot'.format(code))
@@ -897,15 +897,15 @@ class Account(models.Model):
                 return qs_amount
 
     # Return sum of open orders size
-    def get_open_orders_futu(self, code, side, actions):
+    def get_open_orders_futu(self, code, side, action):
 
         # Test if a close_spot or a buy_spot order is open
-        market, flip = self.exchange.get_spot_market(code, self.quote)
+        market, flip = self.exchange.get_perp_market(code, self.quote)
         qs = Order.objects.filter(account=self,
                                   status__in=['open', 'preparation'],
                                   market=market,
                                   side=side,
-                                  actions__in=actions
+                                  actions=action
                                   )
         if not qs.exists():
             log.info('No pending order found for {0} in future'.format(code))

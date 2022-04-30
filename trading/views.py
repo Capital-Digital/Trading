5fbcad3e04c050c349df558cd8272a23c4418c8d
Florent <f.wozniak@protonmail.ch>
2022-04-30 11:23:49 +0200

    fix

--- trading/views.py
+++ trading/views.py
@@ -61,11 +61,9 @@ class AccountDetailView(SingleTableMixin, generic.DetailView):
                                     market__type='spot', year=get_year(), semester=get_semester())
         x = [k for k in assets.keys()]
         bench = [bench.data[k]['last'] for k in bench.data.keys() if k in x]
-        data_assets_hist.append(go.Line(x=x, y=data, name='Balance', line=dict(color='#a9a9a9', width=2)
-                                        )
-                                )
-        data_assets_hist.append(go.Line(x=x, y=bench, name='Bitcoin', line=dict(color='#ff8c00', width=2))
-                                )
+
+        data_assets_hist.append(go.Line(x=x, y=data, name='Balance', secondary_y=False, line=dict(color='#a9a9a9', width=2)))
+        data_assets_hist.append(go.Line(x=x, y=bench, name='Bitcoin', secondary_y=True, line=dict(color='#ff8c00', width=2)))
 
         layout_weights = {
             'yaxis_title': 'Account balance',
@@ -75,7 +73,6 @@ class AccountDetailView(SingleTableMixin, generic.DetailView):
         }
         plot_div_1 = plot({'data': data_assets_hist, 'layout': layout_weights}, output_type='div',
                           )
-
 
         context['plot_div_1'] = plot_div_1
         context['table_asset'] = table_asset

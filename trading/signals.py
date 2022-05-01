35360152c33b961d9e0cc2b8c604c4fd78eb4228
Florent <f.wozniak@protonmail.ch>
2022-05-01 20:41:56 +0200

    fix

--- trading/signals.py
+++ trading/signals.py
@@ -22,6 +22,7 @@ def task_postrun_handler(task_id=None, task=None, args=None, state=None, retval=
 
     if task.name == 'Trading_____Rebalance_account':
 
+        log.info('signal received, sync. complete')
         account_id = args
         account = Account.objects.get(id=account_id)
 

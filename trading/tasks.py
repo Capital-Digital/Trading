1f56b2c5cfe1a5a241351339cbc67d9b6251dc79
Florent <f.wozniak@protonmail.ch>
2022-05-02 10:33:12 +0200

    fix

--- trading/tasks.py
+++ trading/tasks.py
@@ -686,7 +686,7 @@ def market_close(account_id):
 
         log.info('Close position {0}'.format(pos.market.symbol))
 
-        amount = pos.amount
+        amount = pos.size
         if amount > 0:
             side = 'sell'
         else:

4cace4ec4f8efe38aebafdcbbdb9d6e72e6e9931
Florent <f.wozniak@protonmail.ch>
2022-05-05 09:52:57 +0200

    fix

--- marketsdata/models.py
+++ marketsdata/models.py
@@ -654,7 +654,8 @@ class Exchange(models.Model):
             df.columns = pd.MultiIndex.from_product([[market_type], df.columns, [col]])
             df.index = pd.to_datetime(df.index, format="%Y-%m-%dT%H:%M:%SZ", utc=True)
             axis = 1 if col not in df.columns.get_level_values(2) else 0
-            print(axis)
+            print(df.columns.get_level_values(2))
+            print(col, axis)
             self.df = pd.concat([self.df, df], axis=axis)
 
         # Group by columns

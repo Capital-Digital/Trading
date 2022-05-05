903357138d68c77fff16f08d8d3d84f15efe744b
Florent <f.wozniak@protonmail.ch>
2022-05-05 10:29:25 +0200

    fix

--- marketsdata/models.py
+++ marketsdata/models.py
@@ -657,6 +657,9 @@ class Exchange(models.Model):
 
             df.columns = pd.MultiIndex.from_product([[market_type], df.columns, [col]])
             df.index = pd.to_datetime(df.index, format="%Y-%m-%dT%H:%M:%SZ", utc=True)
+
+            print(df)
+            
             if self.df.columns.nlevels == 1:
                 axis = 1
             else:
